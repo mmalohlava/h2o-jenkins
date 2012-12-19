c6dc085e39b3d6f043a9c54542174af89a78a1ae
mmalohlava <michal.malohlava@gmail.com>
2012-12-19 08:31:32 +0100

    testing

--- h2o-dashboard/src/main/java/water/jenkins/plugins/dashboard/tests/TestRunResults.java
+++ h2o-dashboard/src/main/java/water/jenkins/plugins/dashboard/tests/TestRunResults.java
@@ -8,19 +8,42 @@ import java.util.ArrayList;
 public class TestRunResults {
   
   public static final TestRunResults EMPTY_RESULT = new TestRunResults(null);
-  
-  private ArrayList<String> failedTests = new ArrayList<String>();
+
+  private ArrayList<CaseResult> okTests      = new ArrayList<CaseResult>();
+  private ArrayList<CaseResult> failedTests  = new ArrayList<CaseResult>();
+  private ArrayList<CaseResult> skippedTests = new ArrayList<CaseResult>(); 
+  private ArrayList<CaseResult> tests        = new ArrayList<CaseResult>();
   private Run run;
   
   public TestRunResults(Run run) {
     this.run = run;    
   }
 
-  public void addFailedTest(CaseResult cr) {
-    failedTests.add(cr.getName());            
+  public void addTest(CaseResult cr) {
+    tests.add(cr);     
+    if (cr.isPassed()) {
+      okTests.add(cr);
+    } else if (cr.isSkipped()) {
+      skippedTests.add(cr);
+    } else {
+      failedTests.add(cr);
+    }
   } 
   
-  public ArrayList<String> getFailedTests() {
+  public ArrayList<CaseResult> getTests() {
+    return tests;
+  }
+  
+  public ArrayList<CaseResult> getFailedTests() {
     return failedTests;
   }
+  
+  public ArrayList<CaseResult> getOkTests() {
+    return okTests;
+  }
+  public ArrayList<CaseResult> getSkippedTests() {
+    return skippedTests;
+  }
+  
+  
 }
--- h2o-dashboard/src/main/java/water/jenkins/plugins/dashboard/tests/TestUtils.java
+++ h2o-dashboard/src/main/java/water/jenkins/plugins/dashboard/tests/TestUtils.java
@@ -2,6 +2,8 @@ package water.jenkins.plugins.dashboard.tests;
 
 import hudson.maven.reporters.SurefireAggregatedReport;
 import hudson.model.Run;
+import hudson.tasks.junit.SuiteResult;
+import hudson.tasks.junit.TestResult;
 import hudson.tasks.junit.TestResultAction;
 import hudson.tasks.junit.CaseResult;
 
@@ -9,8 +11,10 @@ public class TestUtils {
   
   protected static TestRunResults getTestRunResults(Run run, TestResultAction tra) {
     TestRunResults trr = new TestRunResults(run);
-    for (CaseResult cr : tra.getFailedTests()) {
-      trr.addFailedTest(cr);      
+    TestResult tr = tra.getResult();
+    for (SuiteResult sr : tr.getSuites()) {
+      for (CaseResult cr : sr.getCases())
+      trr.addTest(cr);      
     }
     
     return trr;        
