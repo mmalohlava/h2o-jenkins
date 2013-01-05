99d495e3349a223f77122a2fa3fd29028a131975
mmalohlava <michal.malohlava@gmail.com>
2013-01-05 07:28:21 +0100

    pom version

--- h2o-dashboard/install.sh
+++ h2o-dashboard/install.sh
@@ -3,7 +3,9 @@
 # @Last Change: 24-Dez-2004.
 # @Revision:    0.0
 
-mvn install -Dmaven.test.skip=true
+mvn release:prepare
+#mvn versions:set -DnewVersion=1.0.1-SNAPSHOT
+#mvn install -Dmaven.test.skip=true
 
 java -jar ./sandbox/jenkins-cli.jar -s http://23.23.182.217:8080 install-plugin ~/.m2/repository/org/jenkins-ci/plugins/h2o-dashboard/1.0-SNAPSHOT/h2o-dashboard-1.0-SNAPSHOT.hpi
 
