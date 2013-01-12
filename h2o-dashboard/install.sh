d26434d37a1d14a51d07a41acee9470f89d961a7
mmalohlava <michal.malohlava@gmail.com>
2013-01-12 08:49:53 +0100

    most consie view

--- h2o-dashboard/install.sh
+++ h2o-dashboard/install.sh
@@ -7,5 +7,7 @@ mvn release:prepare
 #mvn versions:set -DnewVersion=1.0.1-SNAPSHOT
 #mvn install -Dmaven.test.skip=true
 
-java -jar ./sandbox/jenkins-cli.jar -s http://23.23.182.217:8080 install-plugin ~/.m2/repository/org/jenkins-ci/plugins/h2o-dashboard/1.0-SNAPSHOT/h2o-dashboard-1.0-SNAPSHOT.hpi
+#PLUGIN_FILE=~/.m2/repository/org/jenkins-ci/plugins/h2o-dashboard/1.0-SNAPSHOT/h2o-dashboard-1.0-SNAPSHOT.hpi
+PLUGIN_FILE=target/h2o-dashboard.hpi
+java -jar ./sandbox/jenkins-cli.jar -s http://23.23.182.217:8080 install-plugin "$PLUGIN_FILE"
 
